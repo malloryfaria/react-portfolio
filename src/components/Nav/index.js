041667a1c07c37b276dd909df8392504985e6139
Mallory <mallory.faria@gmail.com>
2021-04-23 16:15:14 -0400

    fixed bug with home button

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,7 +18,7 @@ function Nav(props) {
   return (
     <header className="flex-row px-1">
       <h3>
-        <a data-testid="link" href="/">
+        <a data-testid="link" href="/react-portfolio/#about">
           <span role="img" aria-label="folder"> 📁</span> Mallory's Portfolio
         </a>
       </h3>

445d8bff93e286ab12db747a06d1caeb5c06c8b5
Mallory <mallory.faria@gmail.com>
2021-04-23 16:21:40 -0400

    fixed issue with header

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,7 +18,7 @@ function Nav(props) {
   return (
     <header className="flex-row px-1">
       <h3>
-        <a data-testid="link" href="/react-portfolio/#about">
+        <a data-testid="link" href="https://malloryfaria.github.io/react-portfolio/#about">
           <span role="img" aria-label="folder"> 📁</span> Mallory's Portfolio
         </a>
       </h3>

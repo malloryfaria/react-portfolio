42fa3f28c7059aa3629dac142e5498ab974c60e4
Mallory <mallory.faria@gmail.com>
2021-04-23 15:30:59 -0400

    fixed issue with opacity on hover

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -18,7 +18,7 @@ function Nav(props) {
   return (
     <header className="flex-row px-1">
       <h3>
-        <a data-testid="link" href="#about">
+        <a data-testid="link" href="/">
           <span role="img" aria-label="folder"> 📁</span> Mallory's Portfolio
         </a>
       </h3>

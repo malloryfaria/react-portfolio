b9a4be67e16d8df0b584a8ce20c0cae723f464b4
Mallory <mallory.faria@gmail.com>
2021-04-23 14:20:45 -0400

    updated gallery to have 6 projects and included github url

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
 function Modal({ onClose, currentPhoto }) {
-    const {name, category, description, index, url} = currentPhoto;
+    const {name, category, description, index, url, githubURL} = currentPhoto;
     
   
     return (
       <div className="modalBackdrop">
         <div className="modalContainer">
           <h3 className="modalTitle">{name}</h3>
+
           <a href={url}>{url}</a><br />
+
+          <a href={githubURL}>{githubURL}</a><br />
+
           <p>{description}</p><br />
           
           <img src={require(`../../assets/large/${category}/${index}.jpg`).default} alt="current category" />
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -10,37 +10,50 @@ const PhotoList = ({ category }) => {
       name: 'Password Generator',
       category: 'projects',
       description: 'This site is a strong password generator tool',
-      url: 'https://malloryfaria.github.io/password-generator/'
+      url: 'https://malloryfaria.github.io/password-generator/',
+      githubURL: 'https://github.com/malloryfaria/password-generator'
     },
     {
       name: 'Tech Blog',
       category: 'projects',
       description: 'A CMS-style blog site where developers can publish their blog posts and comment on other developers’ posts as well. This app uses Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.',
-      url: 'https://mallory-tech-blog.herokuapp.com/'
+      url: 'https://mallory-tech-blog.herokuapp.com/',
+      githubURL: 'https://github.com/malloryfaria/tech-blog'
     },
     {
       name: 'Weather Dashboard',
       category: 'projects',
       description: 'A weather dashboard with a 5 day forecast that will run in the browser and feature dynamically updated HTML and CSS.',
-      url: 'https://malloryfaria.github.io/weather-dashboard/'
+      url: 'https://malloryfaria.github.io/weather-dashboard/',
+      githubURL: 'https://github.com/malloryfaria/weather-dashboard'
     },
     {
       name: 'PokeParte',
       category: 'projects',
       description: 'PokéParté is a website that creates a roster of Pokémon based on your location and the current weather data. Pokémon have unique skills based on their “type” and have higher attacks in certain weather conditions. A Pokémon player would find it interesting and fun to see what kind of Pokémon they could catch near them if they existed in real life.',
-      url: 'https://damn-ma-6.github.io/Poke-Parte/'
+      url: 'https://damn-ma-6.github.io/Poke-Parte/',
+      githubURL: 'https://github.com/damn-ma-6/Poke-Parte'
     },
     {
       name: 'mnmlize',
       category: 'projects',
       description: 'Use this site to organize your life and live a minimalist lifestyle.',
-      url: 'https://mnmlize.herokuapp.com/'
+      url: 'https://mnmlize.herokuapp.com/',
+      githubURL: 'https://github.com/P-I-M/Personal-Inventory-Management-'
+    },
+    {
+      name: 'PWA Budget Tracker',
+      category: 'projects',
+      description: "This is a budget tracker application that allows for offline access and functionality. The user will be able to add expenses and deposits to their budget with or without a connection. If the user enters transactions offline, the total should be updated when they're brought back online.",
+      url: 'https://frozen-wildwood-23744.herokuapp.com/',
+      githubURL: 'https://github.com/malloryfaria/pwa-budget-tracker'
     },
     {
       name: 'Resumé',
       category: 'resumé',
       description: 'Mallory is Certified Salesforce Administrator with a demonstrated history of working in the tech industry. Skilled in Customer Relationship Management, Microsoft Excel, Salesforce Administration, Technical Support and Customer Service. Strong in information technology, graduated with an A.E.C. in Building Services focused in Architecture Technology from Montreal Technical College. She is currently studying full-stack web development at UofT.',
-      url: 'https://www.linkedin.com/in/malloryfaria/'
+      url: 'https://www.linkedin.com/in/malloryfaria/',
+      githubURL: ''
     }
   ]);
 
